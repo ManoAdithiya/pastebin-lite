800a90d33694f1b6c3116106e156e841b0158b08
Mano Adithiya V <manoadithiya2002e@gmail.com>
2026-01-28 19:48:59 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,9 @@ app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB connected"))
+  .then(() => console.log("MongoDB connected"))
   .catch((err) => {
-    console.error("❌ MongoDB connection error:", err);
+    console.error("MongoDB connection error:", err);
     process.exit(1);
   });
 
