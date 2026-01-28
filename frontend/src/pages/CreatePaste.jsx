7b043af282a721de234509ce3bc9a1dd9f7f50d7
Mano Adithiya V <manoadithiya2002e@gmail.com>
2026-01-28 20:53:28 +0530

    Updated

--- frontend/src/pages/CreatePaste.jsx
+++ frontend/src/pages/CreatePaste.jsx
@@ -7,7 +7,7 @@ export default function CreatePaste() {
   async function submit(e) {
     e.preventDefault();
 
-    const res = await fetch("https://pastebin-lite-n3r7.onrender.com/api/pastes", {
+    const res = await fetch("https://pastebin-lite-f3uh.onrender.com/api/pastes", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ content }),
